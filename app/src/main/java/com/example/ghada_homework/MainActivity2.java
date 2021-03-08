4af9f47c11fc782632d78ad290b12ba2e50bea29
ghada <galshowaier@alfaisal.edu>
2021-03-08 12:33:34 +0300

    testing

--- app/src/main/java/com/example/ghada_homework/MainActivity2.java
+++ app/src/main/java/com/example/ghada_homework/MainActivity2.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    int age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +20,22 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         Button show = (Button) findViewById(R.id.button5);
+        final EditText name = (EditText) findViewById(R.id.editTextTextPersonName);
+        final EditText year = (EditText) findViewById(R.id.editTextNumber);
+        final TextView result = ((TextView)findViewById(R.id.textView4));
+        RadioButton male = (RadioButton) findViewById(R.id.radioButton);
 
         show.setOnClickListener(new View.OnClickListener() {
-            final TextView result = ((TextView)findViewById(R.id.textView4));
             @Override
             public void onClick(View v) {
+                age = 2021 - Integer.parseInt(year.getText().toString());
 
-                result.setText("Hi " +R.id.editTextTextPersonName + " You are "+R.id.editTextNumber +" years old ");
+                if(male.isChecked()){
+                    result.setText("Hi Mr " + name.getText() + " You are "+ age +" years old ");
+                }
+                else {
+                    result.setText("Hi Ms " + name.getText() + " You are " + age + " years old ");
+                }
 
             }
         });
